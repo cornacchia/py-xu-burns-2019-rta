116552202bed3d6dea92d0a7c1087cb3965f68da
cornacchia <pozzan.gabriele@gmail.com>
2019-10-31 11:46:46 +0100

    Change plt.subplots.figsize for result images

--- plot.py
+++ plot.py
@@ -4,15 +4,12 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
 def plot_data (results, x, y, output):
-  fig, ax = plt.subplots()
+  fig, ax = plt.subplots(figsize=(12,9))
   for result in results:
     data = result['data']
     ax.plot([data[i][0] for i in range(len(data))], [data[i][1] for i in range(len(data))], label=result['label'])
   ax.set(xlabel=x, ylabel=y)
   ax.grid()
-  #ax.xaxis.set_major_locator(ticker.MultipleLocator(500))
-  #ax.yaxis.set_major_locator(ticker.MultipleLocator(500))
   plt.xticks()
   plt.legend()
-  #plt.savefig(output)
-  plt.show()
+  plt.savefig(output)
